f0f0faf3ae087f0185d75c178e3548435584a34f
osparking <jbpark03@gmail.com>
2023-11-04 06:58:08 +0900

    개발전 - 총 4개의 종속성 추가

---
+++ src/main/java/sq_ch13_ex2/SqCh13Ex2Application.java
@@ -0,0 +1,13 @@
+package sq_ch13_ex2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh13Ex2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh13Ex2Application.class, args);
+	}
+
+}
---
+++ src/test/java/sq_ch13_ex2/SqCh13Ex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sq_ch13_ex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh13Ex2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
