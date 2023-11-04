4c793f62dfdf299a65fa7fe02fa2a2dfc419a2a0
osparking <jbpark03@gmail.com>
2023-11-04 18:38:32 +0900

    RowMapper 인터' 구현 Account 사상기

---
+++ src/main/java/sq_ch13_ex2/repository/AccountMapper.java
@@ -0,0 +1,21 @@
+package sq_ch13_ex2.repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import sq_ch13_ex2.dto.Account;
+
+public class AccountMapper implements RowMapper<Account> {
+
+	@Override
+	public Account mapRow(ResultSet rs, int rowNum) throws SQLException {
+		var account = new Account();
+		account.setId(rs.getInt("id"));
+		account.setName(rs.getString("name"));
+		account.setAmount(rs.getBigDecimal("amount"));
+		return null;
+	}
+
+}
