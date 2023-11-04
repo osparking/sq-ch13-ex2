c2e1cb659a1f4f6a7c012285019c4582cfaf2d7e
osparking <jbpark03@gmail.com>
2023-11-04 19:34:15 +0900

    Rest 종점 2 개 액션 메소드: 목록, 이체

---
+++ src/main/java/sq_ch13_ex2/controller/AccountController.java
@@ -0,0 +1,32 @@
+package sq_ch13_ex2.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import sq_ch13_ex2.dto.Account;
+import sq_ch13_ex2.dto.TransferRequest;
+import sq_ch13_ex2.service.AccountService;
+
+@RestController
+@AllArgsConstructor
+public class AccountController {
+	private final AccountService service;
+	
+	@PostMapping("/transfer") 
+	public void transferMoney(TransferRequest transfer) {
+		service.transferMoney(
+				transfer.getSndrId(),
+				transfer.getRcvrId(), 
+				transfer.getAmount());
+	}
+	
+	@GetMapping("/accounts") 
+	public List<Account> getAllAccounts() {
+		return service.getAllAccounts();
+	}
+	
+}
