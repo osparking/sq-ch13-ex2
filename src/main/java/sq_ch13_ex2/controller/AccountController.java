886c3714a05fd9f2c7f4d31b0a9f3bf8bacd7ba0
osparking <jbpark03@gmail.com>
2023-11-04 21:54:37 +0900

    제어기가 실행시간 예외를 매끄럼게 처리

--- src/main/java/sq_ch13_ex2/controller/AccountController.java
+++ src/main/java/sq_ch13_ex2/controller/AccountController.java
@@ -1,6 +1,7 @@
 package sq_ch13_ex2.controller;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
+import space.jbpark.utility.MyUtil;
 import sq_ch13_ex2.dto.Account;
 import sq_ch13_ex2.dto.TransferRequest;
 import sq_ch13_ex2.service.AccountService;
@@ -16,13 +18,18 @@ import sq_ch13_ex2.service.AccountService;
 @AllArgsConstructor
 public class AccountController {
 	private final AccountService service;
+	private final Logger logger = MyUtil.getLogger(AccountController.class.getName());
 	
 	@PostMapping("/transfer") 
 	public void transferMoney(@RequestBody TransferRequest transfer) {
+		try {
 		service.transferMoney(
 				transfer.getSndrId(),
 				transfer.getRcvrId(), 
 				transfer.getAmount());
+		} catch (Exception e) {
+			logger.info(e.getMessage());
+		}
 	}
 	
 	@GetMapping("/accounts") 
